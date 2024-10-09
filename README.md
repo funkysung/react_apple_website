f2ab2f5237db4406d13ddcc7a006d0f8877dab67
Sung Kim <sungkim7422@gmail.com>
2024-10-08 23:09:27 -0400

    added proper readme, polished the hero section (video)

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # React + Vite
+This is a frontend development project recreating the apple website
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Modern development tools are used to learn their fundamentals
+-GSAP (javascript animation library)
+-node js
+-React (javascript webdev library) -> via vite
+- 3JS (javascript 3D component library)
+-tailwindcss (css framework)
+-basic html components
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Initial tutorial based on https://www.youtube.com/watch?v=RbxHZwFtRT4&list=PL6QREj8te1P4-o8tvI3RF5NOEe0Amzd30&index=3
--- src/constants/index.js
+++ src/constants/index.js
@@ -9,13 +9,13 @@ import {
     yellowImg,
   } from "../utils";
   
-  export const navLists = ["Store", "Mac", "iPhone", "Support"];
+  export const navLists = ["Store", "Smac", "sPhone", "Support"];
   
   export const hightlightsSlides = [
     {
       id: 1,
       textLists: [
-        "Enter A17 Pro.",
+        "Enter s17 Pro.",
         "Game‑changing chip.",
         "Groundbreaking performance.",
       ],
@@ -31,9 +31,9 @@ import {
     {
       id: 3,
       textLists: [
-        "iPhone 15 Pro Max has the",
+        "sPhone 15 Pro has the",
         "longest optical zoom in",
-        "iPhone ever. Far out.",
+        "sPhone ever. Far out.",
       ],
       video: highlightThirdVideo,
       videoDuration: 2,
@@ -49,25 +49,25 @@ import {
   export const models = [
     {
       id: 1,
-      title: "iPhone 15 Pro in Natural Titanium",
+      title: "sPhone 15 Pro in Natural Titanium",
       color: ["#8F8A81", "#ffe7b9", "#6f6c64"],
       img: yellowImg,
     },
     {
       id: 2,
-      title: "iPhone 15 Pro in Blue Titanium",
+      title: "sPhone 15 Pro in Blue Titanium",
       color: ["#53596E", "#6395ff", "#21242e"],
       img: blueImg,
     },
     {
       id: 3,
-      title: "iPhone 15 Pro in White Titanium",
+      title: "sPhone 15 Pro in White Titanium",
       color: ["#C9C8C2", "#ffffff", "#C9C8C2"],
       img: whiteImg,
     },
     {
       id: 4,
-      title: "iPhone 15 Pro in Black Titanium",
+      title: "sPhone 15 Pro in Black Titanium",
       color: ["#454749", "#3b3b3b", "#181819"],
       img: blackImg,
     },
