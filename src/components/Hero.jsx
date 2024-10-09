d456fec801d8a0f391e7bbc3854393cf4f5352eb
Sung Kim <sungkim7422@gmail.com>
2024-10-08 22:09:33 -0400

    added basic styping for navbar.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='w-ful nav-height bg-black relative'>
+        <div className='h-5/6 w-full flex-center flex-col'>
+            <p className='hero-title'>
+                iphone
+            </p>
+
+        </div>
+    </section>
+  )
+}
+
+export default Hero
